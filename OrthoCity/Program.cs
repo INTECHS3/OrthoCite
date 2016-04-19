bac38deb709f9c34bf574fe240b4e5743ace0baa
Marvin Roger <bonjourmarvin@marvinroger.fr>
2016-04-19 12:33:36 +0200

    Initial commit

---
+++ OrthoCity/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OrthoCity
+{
+#if WINDOWS || LINUX
+    /// <summary>
+    /// The main class.
+    /// </summary>
+    public static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new OrthoCity())
+                game.Run();
+        }
+    }
+#endif
+}
