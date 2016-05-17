b62b7cc3030d7939f82085670e89e947864e9f49
Mathieu Boissady <boissady.mathieu@gmail.com>
2016-05-17 09:54:00 +0200

    layer up add in the map

---
+++ OrthoCite/Entities/MiniGames/DoorGame.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+using System;
+using System.Collections.Generic;
+
+namespace OrthoCite.Entities.MiniGames
+{
+    class DoorGame : MiniGame
+    {
+        public DoorGame()
+        {
+        }
+    }
+}
