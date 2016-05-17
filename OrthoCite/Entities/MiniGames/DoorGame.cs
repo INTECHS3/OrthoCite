a8d3f1b8a0bfa4c97808b091d1bb5d3668199038
Valentin <beian77@hotmail.fr>
2016-05-17 10:03:27 +0200

    Fix Compile

--- OrthoCite/Entities/MiniGames/DoorGame.cs
+++ OrthoCite/Entities/MiniGames/DoorGame.cs
@@ -12,6 +12,42 @@ namespace OrthoCite.Entities.MiniGames
     {
         public DoorGame()
         {
+
+        }
+
+        public override void LoadContent(ContentManager content, GraphicsDevice graphicsDevice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void UnloadContent()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Update(GameTime gameTime, KeyboardState keyboardState, Camera2D camera)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Draw(SpriteBatch spriteBatch, Matrix frozenMatrix, Matrix cameraMatrix)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Execute(params string[] param)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal override void Start()
+        {
+
         }
     }
 }
