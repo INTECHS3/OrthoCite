60a69f4f111c2ad465808d4ae9804a500417a3cb
Valentin <beian77@hotmail.fr>
2016-07-04 03:10:10 +0200

    Update introduction In Launcher

---
+++ OrthoCite.Launcher/Introduction_Generic.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace OrthoCite.Launcher
+{
+    partial class Introduction_Generic
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.playerBrowser = new System.Windows.Forms.WebBrowser();
+            this.CountVideo = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // playerBrowser
+            // 
+            this.playerBrowser.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.playerBrowser.Location = new System.Drawing.Point(0, 0);
+            this.playerBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.playerBrowser.Name = "playerBrowser";
+            this.playerBrowser.Size = new System.Drawing.Size(1150, 634);
+            this.playerBrowser.TabIndex = 0;
+            // 
+            // CountVideo
+            // 
+            this.CountVideo.Interval = 1000;
+            this.CountVideo.Tick += new System.EventHandler(this.CountVideo_Tick);
+            // 
+            // Introduction_Generic
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
+            this.ClientSize = new System.Drawing.Size(1150, 634);
+            this.ControlBox = false;
+            this.Controls.Add(this.playerBrowser);
+            this.Name = "Introduction_Generic";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Introduction_Generic";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Introduction_Generic_FormClosing);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.WebBrowser playerBrowser;
+        private System.Windows.Forms.Timer CountVideo;
+    }
+}
---
+++ OrthoCite.Launcher/Introduction_Generic.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Reflection;
+using System.IO;
+
+namespace OrthoCite.Launcher
+{
+    public partial class Introduction_Generic : Form
+    {
+
+        const int limitMin = 2;
+        const int limitSecond = 37;
+        Main _saveMainForm;
+        int Compteur = 0;
+
+        public Introduction_Generic(Main main)
+        {
+            _saveMainForm = main;
+            InitializeComponent();
+            FormBorderStyle = FormBorderStyle.None;
+            WindowState = FormWindowState.Maximized;
+            playerBrowser.Navigate(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\orthocite_intro_final.swf");
+            CountVideo.Start();
+
+        }
+            
+        private void CountVideo_Tick(object sender, EventArgs e)
+        {
+            int totalSecond = limitMin * 60 + limitSecond;
+            Compteur++;
+            if (totalSecond == Compteur) Close();
+        }
+        
+        private void Introduction_Generic_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
