106ac1ed21b775de65e1fef06a89a33c176eb055
Marvin Roger <bonjourmarvin@marvinroger.fr>
2016-07-05 12:21:10 +0200

    Add console also to release

--- OrthoCite/OrthoCite.cs
+++ OrthoCite/OrthoCite.cs
@@ -85,15 +85,14 @@ namespace OrthoCite
 #if DEBUG
             _graphics.PreferredBackBufferWidth = 928;
             _graphics.PreferredBackBufferHeight = 512;
-            AllocConsole();
-            Console.Title = "OCT Debug";
-            System.Console.WriteLine("=== OrthoCite debug console ===");
 #else
             _graphics.PreferredBackBufferWidth = SCENE_WIDTH;
             _graphics.PreferredBackBufferHeight = SCENE_HEIGHT;
             _graphics.IsFullScreen = true;
 #endif
-
+            AllocConsole();
+            Console.Title = "OCT Debug";
+            System.Console.WriteLine("=== OrthoCite debug console ===");
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
             Window.AllowUserResizing = true;
@@ -116,9 +115,6 @@ namespace OrthoCite
             _camera = new Camera2D(_viewportAdapter);
 
             base.Initialize();
-
-
-           
             
         }
 
